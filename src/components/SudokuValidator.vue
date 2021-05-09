5bc0eeba23c8b22233a8c09a65e0c1f601400443
Michal Kowalik <MichalPawel.Kowalik@de.bosch.com>
2021-05-09 12:18:20 +0200

    add empty sudoku component

---
+++ src/components/SudokuValidator.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <p>Sudoku</p>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class SudokuValidator extends Vue {}
+</script>
