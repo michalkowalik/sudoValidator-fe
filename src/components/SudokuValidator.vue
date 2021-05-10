2483dbad98e8d5b1ac0c351bc92bd7d150fec2eb
Michal Kowalik <MichalPawel.Kowalik@de.bosch.com>
2021-05-10 22:51:29 +0200

    v-row based grid

--- src/components/SudokuValidator.vue
+++ src/components/SudokuValidator.vue
@@ -1,6 +1,14 @@
 <template>
   <v-container>
-    <p>Sudoku</p>
+    <v-card width="600" height="600" raised>
+      <v-row v-for="row in sudokuGrid" :key="row">
+        <v-col v-for="cell in row" :key="cell">
+          <v-card>
+            <v-input v-text="cell" />
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
   </v-container>
 </template>
 
@@ -8,5 +16,17 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class SudokuValidator extends Vue {}
+export default class SudokuValidator extends Vue {
+  private sudokuGrid = [
+    [1, 2, 3, 0, 0, 0, 0, 0, 0],
+    [6, 5, 4, 0, 0, 0, 0, 0, 0],
+    [7, 8, 9, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+}
 </script>
