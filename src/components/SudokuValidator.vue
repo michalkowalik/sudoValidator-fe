be5a1d3111643f861cc5986a1a86a224269cd089
Michal Kowalik <MichalPawel.Kowalik@de.bosch.com>
2021-05-10 23:23:06 +0200

    add a simple grid

--- src/components/SudokuValidator.vue
+++ src/components/SudokuValidator.vue
@@ -1,14 +1,20 @@
 <template>
   <v-container>
-    <v-card width="600" height="600" raised>
-      <v-row v-for="row in sudokuGrid" :key="row">
-        <v-col v-for="cell in row" :key="cell">
-          <v-card>
-            <v-input v-text="cell" />
-          </v-card>
-        </v-col>
-      </v-row>
+    <v-card raised>
+      <v-simple-table style="mytable">
+        <tbody>
+          <tr v-for="(row, idx) in sudokuGrid" :key="idx">
+            <td v-for="(cell, idy) in row" :key="idy">
+              <v-text-field dark:true v-model="sudokuGrid[idx][idy]">
+                {{ sudokuGrid[idx][idy] }}
+              </v-text-field>
+            </td>
+          </tr>
+        </tbody>
+      </v-simple-table>
     </v-card>
+    <v-spacer />
+    <v-btn block outlined color="indigo" @click="logGrid"> Validate </v-btn>
   </v-container>
 </template>
 
@@ -18,7 +24,7 @@ import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class SudokuValidator extends Vue {
   private sudokuGrid = [
-    [1, 2, 3, 0, 0, 0, 0, 0, 0],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
     [6, 5, 4, 0, 0, 0, 0, 0, 0],
     [7, 8, 9, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -28,5 +34,16 @@ export default class SudokuValidator extends Vue {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
+
+  private logGrid() {
+    console.log(this.sudokuGrid);
+  }
 }
 </script>
+
+<style scoped>
+.mytable {
+  border-color: black;
+  border: 1em;
+}
+</style>
